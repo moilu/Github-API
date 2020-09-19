8965db81c23eb909cc22eefe6a1a4c5eb7970f2c
Moilu <moisesluna0390@gmail.com>
2020-09-18 18:45:59 -0700

    Proyecto terminado, ya carga datso de la API de Github y acepta nombres de usuario para búsqueda

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-var API = "https://api.github.com/users/";
-const user = "chandzul";
+function getUser() {
+    var user = document.getElementById('u_name').value;
 
-fetch("https://api.github.com/users/"+user)
+    fetch(`https://api.github.com/users/` + user)
+    .then(response => response.json())
+    .then(data => {
+        var name = data.login;
+        var bio = data.bio;
+        var blog = data.blog;
+        var email = data.email;
+        var twitter = data.twitter_username;
+        var repos = data.public_repos;
+        var img_route = data.avatar_url;
+
+        console.log(name) // Prints result from `response.json()` in getRequest
+        console.log(bio)
+        console.log(blog)
+        console.log(email)
+        console.log(twitter)
+        console.log(repos)
+
+        document.getElementById('login').innerHTML = name;
+        document.getElementById('Repositorios').innerHTML = repos;
+        document.getElementById('Bio').innerHTML = bio;
+        document.getElementById('Blog').innerHTML = blog;
+        document.getElementById('Email').innerHTML = email;
+        document.getElementById('Twitter').innerHTML = twitter;
+        document.getElementById('user_pic').src = img_route;
+})
+.catch(error => console.error(error));
+}
+
+fetch(`https://api.github.com/users/` + user)
 .then(response => response.json())
 .then(data => {
     var name = data.login;
